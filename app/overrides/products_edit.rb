9cf66763a64938c58ca8827833d2cc30b6e48d43
wojciech.zywno <wojciech.zywno@goodylabs.com>
2013-06-28 12:58:25 +0200

    bug fixing

--- app/overrides/products_edit.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/admin/products/_form",
-                     :insert_before => "ul#shipping_specs",
-                     :text          => " <div class='field'>
-                        <%= f.check_box :promotion_exclude, :style => 'width:auto;' %>
-                        <%= f.label :promotion_exclude, Spree.t(:promotion_exclude) %>
-                        </div>",
-                     :name          => "products_promotion_exclude")
--- config/initializers/additional_promotions.rb
+++ config/initializers/additional_promotions.rb
@@ -1,19 +1,1 @@
-module Spree
-  class Promotion
-    module Rules
-      class ProductCount < Spree::PromotionRule
-        preference :product_count_quantity_treshold, :decimal, :default => 100.00
-
-        attr_accessible :preferred_product_count_quantity_treshold
-
-        def eligible?(order, options = {})
-        	puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ product_count_quantity_treshold"
-          order.line_items.each { |item| return true if item.quantity >=  preferred_product_count_quantity_treshold  }
-        end
-      end
-
-    end
-  end
-end
-
 Rails.application.config.spree.promotions.rules <<  Spree::Promotion::Rules::ProductCount 
