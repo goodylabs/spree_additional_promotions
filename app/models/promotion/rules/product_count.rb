5b93378aaf455bfde30070dff0e62985f0c52af5
wojciech.zywno <wojciech.zywno@goodylabs.com>
2013-06-28 11:04:25 +0200

    initial commit

---
+++ app/models/promotion/rules/product_count.rb
@@ -0,0 +1,17 @@
+# module Spree
+#   class Promotion
+#     module Rules
+
+#       class ProductCount < Spree::PromotionRule
+#         preference :product_count_quantity_treshold, :decimal, :default => 100.00
+
+#         attr_accessible :preferred_product_count_quantity_treshold
+
+#         def eligible?(order)
+#           order.line_items.each { |item| return true if item.quantity >=  preferred_product_count_quantity_treshold  }
+#         end
+#       end
+
+#     end
+#   end
+# end
---
+++ lib/spree_additional_promotions.rb
@@ -0,0 +1,2 @@
+require 'spree_core'
+require 'spree_additional_promotions/engine'
